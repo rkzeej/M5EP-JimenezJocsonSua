9d1acf8d1e717d21db882a3a8b44513388d14cc1
rkzeej <robbie.kryzz.zeus.jimenez@student.ateneo.edu>
2026-03-15 17:53:22 +0800

    Final submission - Clean code

--- tapasapp/urls.py
+++ tapasapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('basic_list', views.view_basic_list, name='view_basic_list'),
-    path('', views.view_menu, name='view_menu'),
+    path('view_menu', views.view_menu, name='view_menu'),
     path('add_menu', views.add_menu, name='add_menu'),
 ]
