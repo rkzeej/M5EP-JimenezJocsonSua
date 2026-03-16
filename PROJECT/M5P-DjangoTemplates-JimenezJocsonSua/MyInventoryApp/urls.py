410840a79f0b9cf7f423ca999ebdd0f9cdece011
nicsjocson <hnjocson@gmail.com>
2026-03-16 20:08:45 +0800

    steps 2c and 2d

--- PROJECT/M5P-DjangoTemplates-JimenezJocsonSua/MyInventoryApp/urls.py
+++ PROJECT/M5P-DjangoTemplates-JimenezJocsonSua/MyInventoryApp/urls.py
@@ -20,4 +20,6 @@ from . import views
 
 urlpatterns = [
     path('view_supplier', views.view_supplier, name='view_supplier'),
+    path('view_bottles', views.view_bottles, name='view_bottles'),
+    path('add_bottle', views.add_bottle, name='add_bottle'),
 ]
--- PROJECT/M5P-DjangoTemplates-JimenezJocsonSua/MyInventoryApp/views.py
+++ PROJECT/M5P-DjangoTemplates-JimenezJocsonSua/MyInventoryApp/views.py
@@ -8,3 +8,9 @@ def hello_world(request):
 def view_supplier(request):
         supplier_objects = Supplier.objects.all()
         return render(request, 'MyInventoryApp/view_supplier.html', {'suppliers':supplier_objects})
+
+def view_bottles(request):
+    return render(request, 'MyInventoryApp/view_bottles.html')
+
+def add_bottle(request):
+    return render(request, 'MyInventoryApp/add_bottle.html')
